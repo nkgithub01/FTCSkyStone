2a0e3dfe3e6ce20263244cbb86a6fd335f2ba483
nkgithub01 <32886053+nkgithub01@users.noreply.github.com>
2019-11-08 17:31:48 -0800

    code for arms with limits at end of arms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneArmTest.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="ClawArmTest", group="OpMode")
-public class ClawArmTest extends OpMode{
+@TeleOp(name="OneArmTest", group="OpMode")
+public class OneArmTest extends OpMode{
 
     //Objects
     ElapsedTime runtime = new ElapsedTime();
@@ -18,27 +18,28 @@ public class ClawArmTest extends OpMode{
     final int minPos = 100;
     final int maxPos = 4600;
 
+    int initial;
     @Override
     public void init() {
 
         //Initialize DcMotors
         rnpUp1 = hardwareMap.get(DcMotor.class, "rnpUp1");
-        rnpUp2 = hardwareMap.get(DcMotor.class, "rnpUp2");
+        //rnpUp2 = hardwareMap.get(DcMotor.class, "rnpUp2");
 
         //Set zero power behavior
         rnpUp1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rnpUp2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rnpUp2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         //Set directions of the motors
         rnpUp1.setDirection(DcMotor.Direction.FORWARD);
-        rnpUp2.setDirection(DcMotor.Direction.REVERSE);
+        //rnpUp2.setDirection(DcMotor.Direction.REVERSE);
 
         //Set run mode
         rnpUp1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rnpUp2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //rnpUp2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         //Tell user that initialization is complete
         telemetry.addData("Status", "Initialized");
-
+        initial = rnpUp1.getCurrentPosition();
     }
 
     @Override
@@ -52,7 +53,7 @@ public class ClawArmTest extends OpMode{
 
         //Move the arm up and down
         int position1 = rnpUp1.getCurrentPosition();
-        int position2 = rnpUp2.getCurrentPosition();
+        //int position2 = rnpUp2.getCurrentPosition();
         double pwr = -gamepad2.left_stick_y;
         int newPos1 = (int) (pwr * 200) + position1;
         if (minPos < newPos1 && newPos1 < maxPos) {
@@ -60,19 +61,20 @@ public class ClawArmTest extends OpMode{
             rnpUp1.setPower(pwr);
         }
 
-        int newPos2 = (int) (pwr*200) + position2;
+        /*int newPos2 = (int) (pwr*200) + position2;
         if (minPos < newPos2 && newPos2 < maxPos) {
             rnpUp2.setTargetPosition(newPos2);
             rnpUp2.setPower(pwr);
-        }
+        }*/
 
         //Display data
         telemetry.addData("Runtime: ", getRuntime());
-        telemetry.addData("position", position1);
+        telemetry.addData("position", rnpUp1.getCurrentPosition());
         telemetry.addData("new pos", newPos1);
+        telemetry.addData("position change: ", position1 - initial);
 
-        telemetry.addData("position 2: ", position2);
-        telemetry.addData("new pos 2: ", newPos2);
+        //telemetry.addData("position 2: ", position2);
+        //telemetry.addData("new pos 2: ", newPos2);
     }
 
 }
