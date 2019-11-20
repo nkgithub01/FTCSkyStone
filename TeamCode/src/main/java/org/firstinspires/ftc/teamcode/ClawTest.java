8a71e5a015a1dda3c2663c84019bd4bb601cd795
Kevin Zhao <kevinzhao.basis@gmail.com>
2019-11-19 19:09:28 -0800

    Update ClawTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawTest.java
@@ -8,18 +8,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ClawTest extends OpMode {
 
     //Servos
-    Servo claw;
+    //Servo claw;
+    Servo clawLeft;
+    Servo clawRight;
 
     //Variables
-    boolean aPressed;
-    boolean closed;
+    /*boolean aPressed;
+    boolean closed;*/
+
 
 
     @Override
     public void init() {
 
         //Initialize Servo
-        claw = hardwareMap.get(Servo.class, "claw");
+        /*claw = hardwareMap.get(Servo.class, "claw");
 
         //Set servo direction
         claw.setDirection(Servo.Direction.FORWARD);
@@ -29,15 +32,21 @@ public class ClawTest extends OpMode {
 
         //Initialize variables
         aPressed = false;
-        closed = false;
+        closed = false;*/
 
+        clawLeft = hardwareMap.get(Servo.class, "clawLeft");
+        clawRight = hardwareMap.get(Servo.class, "clawRight");
+
+
+        clawRight.setDirection(Servo.Direction.FORWARD);
+        clawLeft.setDirection(Servo.Direction.REVERSE);
     }
 
     @Override
     public void loop(){
 
         //Control claw
-        if(!gamepad1.a) aPressed = false;
+        /*if(!gamepad1.a) aPressed = false;
         if(gamepad1.a && !aPressed)
         {
             aPressed = true;
@@ -55,6 +64,21 @@ public class ClawTest extends OpMode {
 
         //Add data
         telemetry.addData("X: ", gamepad1.left_stick_x);
-        telemetry.addData("y: ", gamepad1.left_stick_y);
+        telemetry.addData("y: ", gamepad1.left_stick_y);*/
+
+        if(gamepad1.right_stick_y > 0)
+        {
+            clawRight.setPosition(1);
+            clawLeft.setPosition(1);
+        }
+        else if(gamepad1.right_stick_y < 0) {
+            clawRight.setPosition(0);
+            clawLeft.setPosition(0);
+        }
+        else {
+            clawRight.setPosition(0.5);
+            clawLeft.setPosition(0.5);
+        }
+
     }
 }
